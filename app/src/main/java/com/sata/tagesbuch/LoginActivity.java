5a62c06d6b29ddcac2bea114fa0e47f6e24571c0
sata <satarupac.59@gmail.com>
2021-08-19 23:21:33 +0200

    SAT1 | Sata | Splashscreen wip

---
+++ app/src/main/java/com/sata/tagesbuch/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.sata.tagesbuch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import java.util.Objects;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+    }
+}
---
+++ app/src/main/java/com/sata/tagesbuch/MainActivity.java
@@ -0,0 +1,30 @@
+package com.sata.tagesbuch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+             Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+             startActivity(intent);
+             finish();
+            }
+        }, 2000);
+    }
+}
